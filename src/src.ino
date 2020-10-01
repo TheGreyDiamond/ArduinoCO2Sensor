e161b4f724d6c3974897ac0882c9c9c7b8bb80a9
TheGreyDiamond <s-einkauf@oest-rs.de>
2020-10-01 20:54:32 +0200

    changes

--- src/src.ino
+++ src/src.ino
@@ -1,19 +1,13 @@
 /*********************************************************************
-  This is an example for our Monochrome OLEDs based on SH1106 drivers
+  IoT CO2 Smart Sensor by TheGreydiamond
+  Thanks to adafruit:
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
 
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-  This example is for a 128x64 size display using I2C to communicate
-  3 pins are required to interface (2 I2C and one reset)
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada  for Adafruit Industries.
-  BSD license, check license.txt for more information
-  All text above, and the splash screen must be included in any redistribution
+    Written by Limor Fried/Ladyada  for Adafruit Industries.
+    BSD license, check license.txt for more information
+    All text above, and the splash screen must be included in any redistribution
 *********************************************************************/
 #include "FS.h"
 #include "SPIFFS.h"
@@ -31,6 +25,7 @@
 #include "RTClib.h"
 #include "AiEsp32RotaryEncoder.h"
 #include <Adafruit_NeoPixel.h>
+#include "icons.c"
 
 #define SDA 21
 #define SCL 22
@@ -40,23 +35,32 @@
 #define ROTARY_ENCODER_BUTTON_PIN 17
 #define ROTARY_ENCODER_VCC_PIN 18
 
+#define VERSION "V1.2.2"
+
 #define FORMAT_SPIFFS_IF_FAILED false
 
-#define LED_PIN     19
-
+#define LED_PIN 19
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT  16
-
+#define LED_COUNT 16
 // NeoPixel brightness, 0 (min) to 255 (max)
 #define BRIGHTNESS 50
 
 int menuPage = 0;
 int menuPageMax = 1;
 int menuPageMin = 0;
+int timeSinceShow = 0;
+
+bool enableLogging = false;
+int lastLog = millis();
+
+String infoText = VERSION;
+int infoIcon = -1;
 
 long timeSinceLastAction = 0;
 bool doneInactivityHandler = false;
 bool doneActivityHandler = false;
+int test_limits = 2;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN);
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
@@ -66,7 +70,6 @@ RTC_DS3231 rtc;
 
 WebServer server(80);
 
-int test_limits = 2;
 void rotary_onButtonClick()
 {
   //rotaryEncoder.reset();
@@ -74,17 +77,21 @@ void rotary_onButtonClick()
   //rotaryEncoder.setBoundaries(-test_limits, test_limits, false);
   //test_limits *= 2;
   Serial.println("CLICK");
+  makeInfoWindow("Knob pressed", 1);
 }
 
-void handleActivity(){
-  if(timeSinceLastAction + 5000 <= millis() && doneInactivityHandler == false){
+void handleActivity()
+{
+  if (timeSinceLastAction + 5000 >= millis() && doneInactivityHandler == false)
+  {
     display.dim(false);
     doneInactivityHandler = true;
     doneActivityHandler = false;
     Serial.println("!!!! Active");
   }
 
-  if(timeSinceLastAction + 5000 >= millis() && doneActivityHandler == false){  // Inactive
+  if (timeSinceLastAction + 5000 <= millis() && doneActivityHandler == false)
+  { // Inactive
     display.dim(true);
     doneInactivityHandler = false;
     doneActivityHandler = true;
@@ -92,7 +99,8 @@ void handleActivity(){
   }
 }
 
-void activity(){
+void activity()
+{
   timeSinceLastAction = millis();
 }
 
@@ -155,30 +163,67 @@ void rotary_loop()
 
         while (file.available())
         {
-
           response->write(file.read());
         }
-      
-
       request->send(response);
     }
 */
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-#define logoVersuch2_black_small_width 32
-#define logoVersuch2_black_small_height 32
-static unsigned char logoVersuch2_black_small_bits[] = {
-    0xff, 0x7f, 0x00, 0x00, 0xff, 0x7f, 0x00, 0x00, 0xff, 0x7f, 0x00, 0x00,
-    0xc0, 0x01, 0x00, 0x00, 0xc0, 0x01, 0x00, 0x00, 0xc0, 0x01, 0x00, 0x00,
-    0xc0, 0x01, 0x00, 0x00, 0xc0, 0x01, 0x00, 0x00, 0xc0, 0x01, 0x00, 0x00,
-    0xc0, 0xf9, 0x03, 0x00, 0xc0, 0xff, 0x0f, 0x00, 0xc0, 0xff, 0x1f, 0x00,
-    0xc0, 0x0f, 0x1e, 0x00, 0xc0, 0x07, 0x08, 0x00, 0xc0, 0x03, 0x00, 0x00,
-    0xc0, 0x01, 0x00, 0x00, 0xc0, 0xc1, 0xff, 0x01, 0xc0, 0xc1, 0xff, 0x1f,
-    0xc0, 0xc1, 0xff, 0x3f, 0xc0, 0x03, 0x1e, 0x7f, 0xc0, 0x03, 0x1e, 0xf8,
-    0xc0, 0x07, 0x1e, 0xf0, 0x80, 0x1f, 0x1f, 0xf0, 0x00, 0xff, 0x1f, 0xe0,
-    0x00, 0xfe, 0x1f, 0xe0, 0x00, 0xf0, 0x1e, 0xe0, 0x00, 0x00, 0x1e, 0xf0,
-    0x00, 0x00, 0x1e, 0xf0, 0x00, 0x00, 0x1e, 0xf8, 0x00, 0x00, 0x1e, 0x7f,
-    0x00, 0x00, 0xfe, 0x3f, 0x00, 0x00, 0xfc, 0x0f};
+void makeInfoWindow(String text, int icon)
+{
+  Serial.println("[LOG] Made info window");
+  timeSinceShow = millis();
+  infoText = text;
+  infoIcon = icon;
+  activity();
+}
+
+boolean handleWindows()
+{
+  if (timeSinceShow + 5000 >= millis())
+  {
+    Serial.println("[LOG] Showing info window");
+    display.clearDisplay();
+    display.invertDisplay(false);
+    display.setTextColor(WHITE);
+    display.setCursor(20, 50);
+    display.setTextColor(WHITE);
+    display.println(infoText);
+    if(infoIcon == 1){
+      display.drawXBitmap(57, 20, warningSign_bits, warningSign_height, warningSign_width, WHITE);
+    }
+    
+    display.display();
+    return (false);
+  }
+  else
+  {
+    return (true);
+  }
+}
+
+void executeLogAction()
+{
+  File fileToAppend = SPIFFS.open("/log.txt", FILE_APPEND);
+  if (!fileToAppend)
+  {
+    Serial.println("There was an error opening the file for appending");
+    makeInfoWindow("Unable to open log file", 1);
+    return;
+  }
+  String logLine = getTimeInLogFormat();
+  logLine += ";";
+  logLine += String(bmp.readTemperature()) + ";" + String(bmp.readHumidity()) + ";" + String(bmp.readPressure()) + "\n";
+  if (fileToAppend.println("APPENDED LINE"))
+  {
+    Serial.println("File content was appended");
+  }
+  else
+  {
+    Serial.println("File append failed");
+    makeInfoWindow("Log saveing failed", 1);
+  }
+}
 
 void handle_NotFound()
 {
@@ -198,6 +243,48 @@ void handle_NotFound()
   server.send(404, "text/html", errorString);
 }
 
+String getTimeInLogFormat(){
+  DateTime now = rtc.now();
+  String out = "";
+   out += now.day();
+  out += ".";
+  out += now.month();
+  out += ".";
+  out += now.year();
+  out += "-";
+  if (String(now.hour()).length() == 1)
+  {
+    out += "0";
+    out += now.hour();
+  }
+  else
+  {
+    out += now.hour();
+  }
+
+  out += ":";
+  if (String(now.minute()).length() == 1)
+  {
+    out += "0";
+    out += now.minute();
+  }
+  else
+  {
+    out += now.minute();
+  }
+  out += ":";
+  if (String(now.second()).length() == 1)
+  {
+    out += "0";
+    out += now.second();
+  }
+  else
+  {
+    out += now.second();
+  }
+  return out
+}
+
 String getTimeAndStuff()
 {
   DateTime now = rtc.now();
@@ -209,31 +296,41 @@ String getTimeAndStuff()
   out += now.year();
   out += " ";
 
-if(String(now.hour()).length() == 1){
+  if (String(now.hour()).length() == 1)
+  {
     out += "0";
     out += now.hour();
-  }else{
+  }
+  else
+  {
     out += now.hour();
   }
-  
+
   out += ":";
-  if(String(now.minute()).length() == 1){
+  if (String(now.minute()).length() == 1)
+  {
     out += "0";
     out += now.minute();
-  }else{
+  }
+  else
+  {
     out += now.minute();
   }
   out += ":";
-  if(String(now.second()).length() == 1){
+  if (String(now.second()).length() == 1)
+  {
     out += "0";
     out += now.second();
-  }else{
+  }
+  else
+  {
     out += now.second();
   }
   return (out);
 }
 
-String getDateOnly(){
+String getDateOnly()
+{
   DateTime now = rtc.now();
   String out = "";
   out += now.day();
@@ -241,7 +338,7 @@ String getDateOnly(){
   out += now.month();
   out += ".";
   out += now.year();
-  return(out);
+  return (out);
 }
 
 String getTimeOnly()
@@ -249,36 +346,44 @@ String getTimeOnly()
   DateTime now = rtc.now();
   String out = "";
 
-  if(String(now.hour()).length() == 1){
+  if (String(now.hour()).length() == 1)
+  {
     out += "0";
     out += now.hour();
-  }else{
+  }
+  else
+  {
     out += now.hour();
   }
-  
+
   out += ":";
-  if(String(now.minute()).length() == 1){
+  if (String(now.minute()).length() == 1)
+  {
     out += "0";
     out += now.minute();
-  }else{
+  }
+  else
+  {
     out += now.minute();
   }
   out += ":";
-  if(String(now.second()).length() == 1){
+  if (String(now.second()).length() == 1)
+  {
     out += "0";
     out += now.second();
-  }else{
+  }
+  else
+  {
     out += now.second();
   }
   return (out);
 }
 
-
 String SendHTML()
 {
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control</title>\n";
+  ptr += "<title>IoT CO2 Sensor</title>\n";
   ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
   ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
@@ -291,7 +396,11 @@ String SendHTML()
   ptr += "</head>\n";
   ptr += "<body>\n";
   ptr += "<h1>IoT CO2 Sensor</h1>\n";
-  ptr += "<h3>" + getTimeAndStuff() + "</h3>\n";
+  ptr += "<h2>Live data</h2>\n";
+  ptr += "Update time: " + getTimeAndStuff() + "<br>\n";
+  ptr += "Temperatur: " + String(bmp.readTemperature()) + "&deg;C<br>\n";
+  ptr += "Luftfeuchte: " + String(bmp.readHumidity()) + "%<br>\n";
+  ptr += "Luftdruck: " + String(bmp.readPressure()) + "Pa<br>\n";
   ptr += "</body></html>";
   return ptr;
 }
@@ -301,11 +410,13 @@ void handleRoot()
   server.send(200, "text/html", SendHTML());
 }
 
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+void colorWipe(uint32_t color, int wait)
+{
+  for (int i = 0; i < strip.numPixels(); i++)
+  {                                // For each pixel in strip...
+    strip.setPixelColor(i, color); //  Set pixel's color (in RAM)
+    strip.show();                  //  Update strip to match
+    delay(wait);                   //  Pause for a moment
   }
 }
 
@@ -320,7 +431,7 @@ void setup()
   display.clearDisplay();
   display.display();
   display.clearDisplay();
-  display.drawXBitmap(48, 10, logoVersuch2_black_small_bits, 32, 32, WHITE);
+  display.drawXBitmap(48, 10, tgd_logo_bits, tgd_logo_height, tgd_logo_width, WHITE);
   display.setCursor(16, 48);
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -330,7 +441,7 @@ void setup()
   if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
   {
     display.clearDisplay();
-    display.drawXBitmap(48, 10, logoVersuch2_black_small_bits, 32, 32, WHITE);
+    display.drawXBitmap(48, 10, tgd_logo_bits, tgd_logo_height, tgd_logo_width, WHITE);
     display.setCursor(16, 48);
     display.setTextSize(1);
     display.setTextColor(WHITE);
@@ -342,52 +453,33 @@ void setup()
 
   WiFi.disconnect();
   WiFi.mode(WIFI_OFF);
-
   WiFi.softAP("IoT CO2 Sensor");
-  
-
-  // bool DNSstate = MDNS.begin("sensor");
-
   server.on("/", handleRoot);
-
-  ///erver.on("/inline", []() {
-  //   server.send(200, "text/plain", "this works as well");
-  //});
-
   server.onNotFound(handle_NotFound);
   server.begin();
-  //MDNS.addService("http", "tcp", 80);
-
   if (!rtc.begin())
   {
     Serial.println("Couldn't find RTC");
     Serial.flush();
-    //abort();
   }
   if (rtc.lostPower())
   {
     Serial.println("RTC lost power, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // September 30, 2020 at 3am you would call:
     //rtc.adjust(DateTime(2020, 9, 30, 21, 37, 30));
   }
 
-  
   rotaryEncoder.begin();
   rotaryEncoder.setup([] { rotaryEncoder.readEncoder_ISR(); });
   //optionally we can set boundaries and if values should cycle or not
-  rotaryEncoder.setBoundaries(0, 10, true); //minValue, maxValue, cycle values (when max go to min and vice versa)
+  rotaryEncoder.setBoundaries(0, menuPageMax * 2, true); //minValue, maxValue, cycle values (when max go to min and vice versa)
 
   bmp.begin(0x76);
   strip.begin();
   strip.show();
   strip.setBrightness(BRIGHTNESS);
-  colorWipe(strip.Color(  0,   0,   0, 255), 50);
-  delay(1000);
-  
+  colorWipe(strip.Color(0, 0, 0, 255), 50);
+  delay(500);
+
   activity();
   Serial.println("Start");
 }
@@ -395,54 +487,55 @@ int i = 2000;
 
 void loop()
 {
-  Serial.println("Start");
   if (i >= 2000)
   {
     Serial.println(menuPage);
-    if (menuPage == 0)
+    if (handleWindows())
     {
-      display.clearDisplay();
-      display.invertDisplay(false);
-      //display.setTextSize(1);
-      display.setTextColor(WHITE);
-      /*display.setCursor(0, 0);
-      display.println("Es sind");
-      display.println("");*/
-      display.setCursor(0, 0);
-      display.setTextSize(2);
-      display.setTextColor(WHITE);
-      String str = String(bmp.readTemperature());
-      str += " C";
-      display.println(str);
-      str = String(bmp.readHumidity());
-      str += " %";
-      display.println(str);
-      str = String(bmp.readPressure());
-      display.print(str);
-      display.setTextSize(1.5);
-      display.println(" Pa");
-      display.display();
-      i = 1990;
-    }else if(menuPage == 1){
-      display.clearDisplay();
-      display.invertDisplay(false);
-      display.setTextSize(2);
-      display.setCursor(16, 20);
-      display.setTextColor(WHITE);
-      display.println(getTimeOnly());
-      display.setCursor(36, 40);
-      display.setTextSize(1);
-      display.println(getDateOnly());
-      display.display();
-      i = 1995;
+      if (menuPage == 0) // Just temperature
+      {
+        display.clearDisplay();
+        display.invertDisplay(false);
+        display.setTextColor(WHITE);
+        display.setCursor(0, 0);
+        display.setTextSize(2);
+        display.setTextColor(WHITE);
+        String str = String(bmp.readTemperature());
+        str += " C";
+        display.println(str);
+        str = String(bmp.readHumidity());
+        str += " %";
+        display.println(str);
+        str = String(bmp.readPressure());
+        display.print(str);
+        display.setTextSize(1.5);
+        display.println(" Pa");
+        display.display();
+        i = 1990;
+      }
+      else if (menuPage == 1)
+      { // Just time
+        display.clearDisplay();
+        display.invertDisplay(false);
+        display.setTextSize(2);
+        display.setCursor(16, 20);
+        display.setTextColor(WHITE);
+        display.println(getTimeOnly());
+        display.setCursor(36, 40);
+        display.setTextSize(1);
+        display.println(getDateOnly());
+        display.display();
+        i = 1995;
+      }
     }
   }
   i++;
-  Serial.println("Loooopyyyy done");
   server.handleClient();
-  Serial.println("Hit 1");
   handleActivity();
   rotary_loop();
   delay(50);
-  if (millis() > 20000){ rotaryEncoder.enable();}
+  if (millis() > 20000)
+  {
+    rotaryEncoder.enable();
+  }
 }
